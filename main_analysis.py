9b1fe3587065288b7ba9c0ec7b156da2053aae9b
FLatino <francesco.latino@ext.luxottica.com>
2026-02-05 11:05:53 +0100

    Implemented RandomizedSearchCV for hyperparameters tuning

--- main_analysis.py
+++ main_analysis.py
@@ -1,18 +1,3 @@
-#-*- coding: utf-8 -*-
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.16.1
-#   kernelspec:
-#     display_name: venv (3.10.11)
-#     language: python
-#     name: python3
-# ---
-
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
